aea7fc5dc45e38e7da84ee361663b47337e77e58
Marcelo <mar.sousa2061@gmail.com>
2020-03-08 01:16:02 -0300

    Adding the config database

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'marcelo',
+    password: 'marcelo123',
+    database: 'nodeSequelize',
+    define: {
+        timestamp: true,
+        underscored: true,
+    },
+}
